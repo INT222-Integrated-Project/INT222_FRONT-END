b4e54ab23015efc49b7e831667945983caa94446
pimpony <56395239+pimpony@users.noreply.github.com>
2021-09-01 15:46:10 +0700

    Carousel

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,65 +1,36 @@
 <template>
-
-
-      
-  <div
-    class="flex justify-center items-center bg-cover bg-center w-screen min-h-screen"
-    style="background-image: url()"
-  >
+  <div class="flex justify-center items-center bg-cover bg-center w-screen min-h-screen">
     <!-- block image to search and can edit , delete  -->
-    <div
-      class="h-screen md:w-4/6 bg-white bg-opacity-50 items-center md:mx-auto rounded-xl mt-16 lg:md-20"
-    >
+    <div class="h-screen md:w-4/6 bg-white bg-opacity-50 items-center md:mx-auto rounded-xl mt-16 lg:md-20">
       <div class="mt-3 text-center">
         <!-- Search -->
-        <div
-          class="flex justify-center items-center text-center flex-col v-show="
-        >
-          <input
-            v-model="inputSearch"
-            placeholder="What are you finding?"
-            class="p-2 bg-white w-full rounded border"
-          />
+        <div class="flex justify-center items-center text-center flex-col v-show=">
+          <input v-model="inputSearch" placeholder="What are you finding?" class="p-2 bg-white w-full rounded border"/>
           <!-- if notfound Show ... -->
-          <p v-if="notFound" class="text-center text-xl mt-4">
-            We can't find the Case
-          </p>
+          <p v-if="notFound" class="text-center text-xl mt-4"> We can't find the Case</p>
+           <slides></slides>
         </div>
-        <showcase 
-        @edit-click="openEditModal" @send-data="editCase"></showcase>
+       
+        <showcase  @edit-click="openEditModal" @send-data="editCase"></showcase>
       </div>
-
-      
     </div>
   </div>
 
-  <inputdata
-    v-if="addClicked"
-    @close="changeAddItemClicked"
-    formLabel="Add New Case"
-  ></inputdata>
+  <inputdata v-if="addClicked"  @close="changeAddItemClicked" formLabel="Add New Case"></inputdata>
+  <inputdata v-if="editClicked" :product="editProducts" @close="changeEditItemClicked" formLabel="Edit Case"></inputdata>
 
-  <inputdata
-    v-if="editClicked"
-    :product="editProducts"
-    @close="changeEditItemClicked"
-    formLabel="Edit Case"
-  ></inputdata>
 </template>
 
 <script>
 import showcase from "../components/ShowCase";
-import inputdata from "../components/AddProduct";
-
-
+import inputdata from "../components/AddandEditProduct.vue";
+import slides from "../components/Caseslideshow";
 export default {
   emits: ["close-add-modal"],
   props: ["addClicked",],
   components: {
     inputdata,
-    showcase,
-    
-    
+    showcase,slides
   },
  data() {
     return {
@@ -69,7 +40,8 @@ export default {
       inputSearch: "",
       notFound: false,
       editProducts: [],
-     
+      slides:false,
+      search: '',
       
     };
   },
@@ -95,6 +67,12 @@ export default {
     editCase(editingCase){
         this.editProducts=editingCase;        
     },
-  }
+  },computed: {
+    filteredList() {
+      return this.postList.filter(post => {
+        return post.title.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  },
 };
 </script>
