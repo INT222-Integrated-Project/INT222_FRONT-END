2d65843cbbc10db416fbc269afb9fdd09f96850c
pimpony <56395239+pimpony@users.noreply.github.com>
2021-09-26 17:15:48 +0700

    change template Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,83 +1,64 @@
-<template>
-  <div class="fixed z-10 inset-0 overflow-y-auto  ">
-    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-      <div
-        class="inline-block align-bottom bg-white rounded-lg text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-3/12">
-          <div class="container ">    
-            <div   class="col-lg-4 col-md-6 col-sm-8 mx-auto ">
-                <div class="bg-black w-full text-white  flex justify-end ">
-                  <router-link to="/" >
-                    <button ><span class="material-icons  px-2 pt-2">close</span></button>
-                  </router-link>   
+<template >
+    <div class="min-h-screen bg-black text-gray-900 flex justify-center">
+    <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+      <div class="lg:w-1/2 xl:w-5/12 p-3 sm:p-12">
+        <div>
+          <img src="https://www.pngkey.com/png/full/83-837247_quick-view-sweet-word.png" class="w-32 mx-auto"/>
+        </div>
+        <div v-if="!registerActive" key=""  v-bind:class="{ error: emptyFields }" class="mt-12 flex flex-col items-center">
+          <h1 class="text-2xl xl:text-3xl font-extrabold">
+            Sign In
+          </h1>
+            <form @submit.prevent="doLogin" class="w-full flex-1 mt-3"> 
+                <div class="mx-auto max-w-xs">
+                    <input v-model="userLogin" type="text" name="username" placeholder="Username" required class="input-text"/>
+                    <input v-model="passwordLogin"  type="password" name="password" placeholder="Password"  required class="input-text"/>
+                    <div class="m-1 ">
+                        <input type="submit" class="input-sign-in"  @click="doLogin" />
+                    </div>
+                    <div class=" m-2">  
+                        <p class="items-center mt-5 text-center">Don't have an account? </p>
+                    <a  href="#"  @click="  (registerActive = !registerActive),  (emptyFields = false) " class="input-sign-up ">
+                        <i class="material-icons items-center">person_add_alt_1</i>  
+                        <h3 class="ml-3"> Sign Up</h3> 
+                    </a>   
+                    </div>
                 </div>
-              <div v-if="!registerActive" key="" class="" v-bind:class="{ error: emptyFields }">
-                <div class=" bg-black rounded -my-5  ">
-                  <div class="flex mx-5 p-5 items-start text-white ">
-                    <span class="material-icons">lock</span>
-                    <h1 class="ml-2">Sign In</h1>
-                  </div>
-                </div> 
-                <form @submit.prevent="doLogin" class="flex flex-col text-gray-400 m-5 rounded-xl">
-                  <div class="m-1 mt-2">
-                    <p class="flex items-start">Username</p>
-                    <input v-model="userLogin" type="text" name="username"  class=" w-full bg-gray-300 h-8" placeholder="" required/>
-                  </div>
-                  <div class="m-1 mt-2  ">
-                    <p class="flex items-start">Password</p>
-                    <input  v-model="passwordLogin"  type="password" name="password" class="w-full  bg-gray-300 h-8"  placeholder=""  required/>
-                  </div>
-                  <div class="m-1 ">
-                    <input type="submit" class="bg-pink-500  p-2 text-white "  @click="doLogin" />
-                  </div>
-                    <p class="flex flex-col">  Don't have an account? <a  href="#"  @click="  (registerActive = !registerActive),  (emptyFields = false) " class="text-red-600 underline">Create one now </a></p>
+            </form> 
+        </div>
+        <div  v-else  class="mt-12 flex flex-col items-center"  v-bind:class="{ error: emptyFields }">
+            <h1 class="text-2xl xl:text-3xl font-extrabold">
+                Sign Up
+            </h1> 
+            <form class="flex flex-col text-gray-400 m-5   rounded-xl">
+                <input v-model="fnameReg" type="fname" name="Firstname" placeholder="Firstname" required class="input-text"/>
+                <input v-model="lnameReg"  type="lname" name="Lastname" placeholder="Lastname"  required class="input-text"/>
+                <input v-model="userReg" type="userReg" name="Username" placeholder="Username" required class="input-text"/>
+                <input v-model="passwordReg"  type="password" name="Password" placeholder="Password"  required class="input-text"/>
+                <input v-model="phoneReg" type="phoneReg" name="Phone" placeholder="Phone" required class="input-text"/> 
+                <div class="m-1 ">
+                    <input type="submit" class="input-sign-in"  @click="doRegister" />
+                </div>
+                <p class="items-center mt-5 text-center"> Already have an account? </p>  
+                <a href="#" @click="  (registerActive = !registerActive),  (emptyFields = false)"  class="input-sign-up">
+                    <i class="material-icons items-center">person</i>  
+                        <h3 class="ml-3"> Sign In</h3>
+                </a>    
                 </form>
               </div>
-
-              <div  v-else class=""  v-bind:class="{ error: emptyFields }">
-                <div class=" bg-black rounded -my-5  ">
-                  <div class="flex mx-5 p-5 items-start text-white ">
-                    <button ><span class="material-icons">lock</span></button>
-                    <h1 class="ml-2">Sign Up</h1>
-                  </div>
-                </div> 
-
-                <form class="flex flex-col text-gray-400 m-5   rounded-xl">
-                   <div class="m-1 mt-2">
-                    <p class="flex items-start">Firstname</p>
-                    <input  v-model="fnameReg" type="fname" class=" w-full bg-gray-300 h-8" placeholder="" required  />
-                  </div>
-                  <div class="m-1 mt-2  ">
-                    <p class="flex items-start">Lastname</p>
-                    <input  v-model="lnameReg"
-                        type="lname"  class="w-full  bg-gray-300 h-8"  placeholder=""  required/>
-                  </div>
-                  <div class="m-1 mt-2">
-                    <p class="flex items-start">Username</p>
-                    <input  v-model="userReg" type="email" class=" w-full bg-gray-300 h-8" placeholder="" required  />
-                  </div>
-                  <div class="m-1 mt-2  ">
-                    <p class="flex items-start">Password</p>
-                    <input  v-model="passwordReg"
-                        type="email"  class="w-full  bg-gray-300 h-8"  placeholder=""  required/>
-                  </div>
-                  <div class="m-1 mt-2  ">
-                    <p class="flex items-start">Phone</p>
-                    <input  v-model="phoneReg"
-                        type="phone"  class="w-full  bg-gray-300 h-8"  placeholder=""  required/>
-                  </div>   
-                  <div class="m-1 ">
-                    <input type="submit" class="bg-pink-500  p-2 text-white "   @click="doRegister" />
-                  </div>
-                    <p class="flex flex-col">  Already have an account? <a href="#" @click="  (registerActive = !registerActive),  (emptyFields = false)"  class="text-red-600 underline">Sign in here </a></p>     
-                    <p></p>
-                </form>
-              </div>
-            </div>
-          </div>
+        <div class="flex justify-center items-center">
+            <a href="#" class="border-b border-gray-500 border-dotted">Terms of Service</a>
+            <p class="mx-2">and its</p>
+            <a href="#" class="border-b border-gray-500 border-dotted">Privacy Policy</a>
         </div>
       </div>
+      <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
+        <div
+          class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+          style="background-image: url('https://cdn140.picsart.com/260643629026212.png?type=webp&to=min&r=640');"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
