c76c9dcff156dfa34faaa4d70c18a1c4795a088a
pimpony <56395239+pimpony@users.noreply.github.com>
2021-11-26 22:11:49 +0700

    edit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="grid grid-cols-1 lg:grid-cols-2  mr-28 ml-24  p-1 mt-2 bg-white ">
         <div class="mb-2 flex justify-center items-center"><slot name = "image"></slot></div>
-      
-        
         <div class="text-left font-bold  flex justify-center items-center flex-col ">
         <h1 class="header text-2xl  lg:text-4xl ">
             <slot name="memberName"></slot>
