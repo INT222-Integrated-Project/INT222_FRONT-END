8d4ce9665d2b5ca91e3849eb88fc28dc1578d528
Nature Gecko <thanaphat.ns@mail.kmutt.ac.th>
2021-12-01 01:19:37 +0700

    Clone the current progress to dev

--- src/components/staffs/StaffOrderList.vue
+++ src/components/staffs/StaffOrderList.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="">
-
-    </div>
+    <div>My orders</div>
 </template>
 
 <script>
 export default {
     setup() {
-        
-    },
+        return{
+
+        }
+    }
 }
 </script>
